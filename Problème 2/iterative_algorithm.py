1b3538b6b3b386403607f910bd3fc057768cab45
Titouan Vial <titouan.vial@grenoble-inp.org>
2024-04-18 15:33:09 +0200

    Changement de structure de la version itérative pour utiliser une matrice (gain temporel)

--- Problème 2/iterative_algorithm.py
+++ Problème 2/iterative_algorithm.py
@@ -1,18 +1,21 @@
 import util
 import collections
 import time
+import numpy as np
 
 
 def iterative_search(words, line_length):
 
-    scores = {}
     n = len(words)
+    max_height = util.find_longest_seq(words, line_length)
+    scores = np.full((n, max_height), float('inf'))
 
     i = n - 1
-    # curr_state is of the form [line_first_word_index, line_last_word_index]
-    curr_state = [i] * 2
+    # curr_state is of the form
+    # [line first word index, number of word on the line after the first one]
+    curr_state = [i, 0]
     curr_len = len(words[i])
-    scores[tuple(curr_state)] = (line_length - curr_len)**2
+    scores[*curr_state] = (line_length - curr_len)**2
     for j in range(i - 1, -1, -1):
         # consider adding word j to the line
         curr_len += 1 + len(words[j])
@@ -20,20 +23,21 @@ def iterative_search(words, line_length):
             break
 
         curr_state[0] = j
-        scores[tuple(curr_state)] = (line_length - curr_len)**2
+        curr_state[1] = i - j
+        scores[*curr_state] = (line_length - curr_len)**2
 
     for i in range(n - 2, -1, -1):
-        curr_state = [i] * 2
+        curr_state = [i, 0]
         curr_len = len(words[i])
         curr_key = tuple(curr_state)
 
-        temp_end = curr_state[1]
-        curr_state[1] = i + 1  # consider adding word i + 1 to the line
-        scores[curr_key] = min(
-            scores[tuple(curr_state)],  # add case
-            (line_length - curr_len)**2 + scores[(i + 1,) * 2]  # newline case
+        # temp_end = curr_state[1]
+        curr_state[1] += 1  # consider adding word i + 1 to the line
+        scores[*curr_key] = min(
+            scores[*curr_state],  # add case
+            (line_length - curr_len)**2 + scores[i + 1, 0]  # newline case
         )
-        curr_state[1] = temp_end  # remove extra word
+        curr_state[1] -= 1  # remove extra word
 
         for j in range(i - 1, -1, -1):
             # consider adding word j to the line
@@ -42,58 +46,58 @@ def iterative_search(words, line_length):
                 break
 
             curr_state[0] = j
+            curr_state[1] = i - j
             # this keeps track of which combination we are looking
             # the score for
             curr_key = tuple(curr_state)
 
             # consider adding word i + 1 to the line
-            temp_end = curr_state[1]
-            curr_state[1] = i + 1  # consider adding word i + 1 to the line
+            # temp_end = curr_state[1]
+            curr_state[1] += 1  # consider adding word i + 1 to the line
             # case where adding word i + 1 overflows the line
             if curr_len + len(words[i + 1]) + 1 > line_length:
-                scores[curr_key] = (line_length - curr_len)**2 + scores[(i + 1,) * 2]
+                scores[*curr_key] = (line_length - curr_len)**2 + scores[i + 1, 0]
             else:  # else compare adding and making a new line
-                scores[curr_key] = min(
-                    scores[tuple(curr_state)],  # add case
-                    (line_length - curr_len)**2 + scores[(i + 1,) * 2]  # newline case
+                scores[*curr_key] = min(
+                    scores[*curr_state],  # add case
+                    (line_length - curr_len)**2 + scores[i + 1, 0]  # newline case
                 )
-            curr_state[1] = temp_end  # remove extra word
+            curr_state[1] -= 1  # remove extra word
 
     i = 0
-    curr_state = [i] * 2
+    curr_state = [i, 0]
     output_lines = [words[i]]
     while i < n - 1:
 
         # suppose we add the previous word, check the operation won't overflow the
         # current line, in which case make a new line
         if len(output_lines[-1]) + len(words[i + 1]) + 1 > line_length:
-            curr_state = [i + 1] * 2
+            curr_state = [i + 1, 0]
             output_lines.append(words[i + 1])
             i += 1
             continue
 
-        temp_end = curr_state[1]
-        curr_state[1] = i + 1  # consider adding word i + 1 to the line
-        add_score = scores[tuple(curr_state)]
-        curr_state[1] = temp_end  # remove extra word
+        curr_state[1] += 1  # consider adding word i + 1 to the line
+        add_score = scores[*curr_state]
+        curr_state[1] -= 1  # remove extra word
         newline_score = (
             util.get_line_score(output_lines[-1], line_length)  # current line score
-            + scores[(i + 1,) * 2]  # next line score
+            + scores[i + 1, 0]  # next line score
         )
 
         # to minise the score, we apply the operation that has minimal score
         if add_score < newline_score:
             output_lines[-1] += ' ' + words[i + 1]
-            curr_state[1] = i + 1
+            curr_state[1] += 1
         else:
             output_lines.append(words[i + 1])
-            curr_state = [i + 1] * 2
+            curr_state = [i + 1, 0]
 
         i += 1
 
     return '\n'.join(
             f'{line:{line_length}}' for line in output_lines
-        ), scores[(0,) * 2]
+        ), scores[0, 0]
 
 
 if __name__ == '__main__':
--- Problème 2/util.py
+++ Problème 2/util.py
@@ -1,3 +1,4 @@
+import collections
 
 
 def import_words_from_text(filename):
@@ -25,6 +26,33 @@ def get_text_score(text, line_length):
     return score
 
 
+def find_longest_seq(words, line_length):
+
+    max_buffer_len = 0
+
+    buffer = collections.deque([])
+    curr_len = 0
+    for i in range(len(words)):
+        if buffer:
+            last_i = buffer.popleft()
+            curr_len -= len(words[last_i]) + 1
+
+        else:
+            buffer.append(i)
+            curr_len += len(words[i])
+
+        for j in range(i, len(words)):
+            if curr_len + len(words[j]) + 1 > line_length:
+                break
+
+            buffer.append(j)
+            curr_len += len(words[j]) + 1
+
+            if len(buffer) > max_buffer_len:
+                max_buffer_len = len(buffer)
+
+    return max_buffer_len
+
 if __name__ == '__main__':
 
     line_length = 80
@@ -33,6 +61,8 @@ if __name__ == '__main__':
     # print(len(text))
     # print(text)
 
-    with open('solution_complet.txt', mode='r') as file:
-        text = file.read()
-    print(get_text_score(text, line_length))
+    # print(find_longest_seq(text, line_length))
+
+    # with open('solution_p1.txt', mode='r') as file:
+    #     text = file.read()
+    # print(get_text_score(text, line_length))
