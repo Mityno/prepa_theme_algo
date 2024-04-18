21c12b04940671417b5458ab450e2e089d5c96d6
Bastien Brogliato--Martin <reddibabas@gmail.com>
2024-04-18 15:17:51 +0200

    ça marche maintenant wala

--- Problème 2/recursive_algorithm.py
+++ Problème 2/recursive_algorithm.py
@@ -4,7 +4,7 @@ import sys
 import time
 
 
-def aux(end_space, index, word_list, L, mem_dict):
+def aux(end_space, index, word_list, L, mem_dict, line_break_dict):
     # Mémoisation :
     if (end_space, index) in mem_dict:
         return mem_dict[(end_space, index)]
@@ -18,20 +18,39 @@ def aux(end_space, index, word_list, L, mem_dict):
     m = word_list[index] + ' '
 
     if len(m) - 1 > end_space:
-        mem_dict[(end_space, index)] = (end_space+1)**2 + aux(L - len(m), index+1, word_list, L, mem_dict)
+        mem_dict[(end_space, index)] = (end_space+1)**2 + aux(L - len(m), index+1, word_list, L, mem_dict, line_break_dict)
+        line_break_dict[(end_space, index)] = True
         return mem_dict[(end_space, index)]
     
 
-    current_line = aux(end_space - len(m), index+1, word_list, L, mem_dict)
-    new_line = (end_space+1)**2 + aux(L - len(m), index+1, word_list, L, mem_dict)
-    mem_dict[(end_space, index)] = min(current_line, new_line)
+    current_line = aux(end_space - len(m), index+1, word_list, L, mem_dict, line_break_dict)
+    new_line = (end_space+1)**2 + aux(L - len(m), index+1, word_list, L, mem_dict, line_break_dict)
+    if current_line <= new_line:
+        mem_dict[(end_space, index)] = current_line
+        line_break_dict[(end_space, index)] = False
+    else:
+        mem_dict[(end_space, index)] = new_line
+        line_break_dict[(end_space, index)] = True
     return mem_dict[(end_space, index)]
 
 
 def least_squares(word_list, L):
     mem_dict = {}
-    least_error = aux(L, 0, word_list, L, mem_dict)
-    return least_error
+    line_break_dict = {}
+    least_error = aux(L, 0, word_list, L, mem_dict, line_break_dict)
+    line_list = [[]]
+    end_space = L
+    for index, word in enumerate(word_list):
+        if line_break_dict[(end_space, index)]:
+            line_list.append([])
+            end_space = L - len(word) - 1
+        else:
+            end_space -= len(word) + 1
+        line_list[-1].append(word)
+
+    line_list = [' '.join(line) for line in line_list]
+    result_text = '\n'.join(line_list)
+    return least_error, result_text
 
 
 if __name__ =='__main__':
@@ -39,8 +58,14 @@ if __name__ =='__main__':
     # print(len(word_list))
     sys.setrecursionlimit(len(word_list)*10)
     deb = time.perf_counter()
-    print(least_squares(word_list, 80))
+    least_error, result_text = least_squares(word_list, 80)
+    print(least_error)
     fin = time.perf_counter()
     print(f'{least_squares.__name__} took {fin - deb:.3f}s to run')
+
+    with open('./Problème 2/solution_complet.txt', mode='w') as file:
+        file.write(result_text)
+    
+
 
     
