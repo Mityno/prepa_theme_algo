daae4d2607b4124dab3392b53128efd380c79ca3
Bastien Brogliato--Martin <reddibabas@gmail.com>
2024-04-18 22:57:00 +0200

    commenté et annoté recursive_algorithm.py

--- Problème 2/recursive_algorithm.py
+++ Problème 2/recursive_algorithm.py
@@ -4,11 +4,12 @@ import sys
 import time
 
 
-def least_squares(word_list, L):
-    mem_dict = {}
-    line_break_dict = {}
+def least_squares(word_list: list[str], L: int) -> tuple[int, str]:
+    mem_dict: dict[tuple[int, int], int] = {}
+    line_break_dict: dict[tuple[int, int], bool] = {}
 
-    def aux(end_space, index):
+    # Définition de la fonction auxiliaire récursive, pour qui les paramètres de la fonction principale sont des variables globales
+    def aux(end_space: int, index: int) -> int:
         # Mémoisation :
         if (end_space, index) in mem_dict:
             return mem_dict[(end_space, index)]
@@ -19,14 +20,19 @@ def least_squares(word_list, L):
             return mem_dict[(end_space, index)]
         
         # Cas général :
+        # NB : les "+1" dans le calcul des scores de fin de ligne servent à compenser l'ajout des espaces en fin de mot 
+        # (le dernier espace de la ligne étant dans un mot, il est soustrait à end_space mais doit être compté)
         m = word_list[index] + ' '
-
+        
+        # Force un retour à la ligne si l'espace en fin de ligne n'est pas suffisant
         if len(m) - 1 > end_space:
             mem_dict[(end_space, index)] = (end_space+1)**2 + aux(L - len(m), index+1)
             line_break_dict[(end_space, index)] = True
             return mem_dict[(end_space, index)]
         
-
+        # Si end_space le permet, compare le score obtenu en effectuant un retour à la ligne et celui
+        # obtenu sans retour, et conserve le meilleur cas des deux, en notant si un retour à la ligne
+        # a été effectué ou non
         current_line = aux(end_space - len(m), index+1)
         new_line = (end_space+1)**2 + aux(L - len(m), index+1)
         if current_line <= new_line:
@@ -38,10 +44,14 @@ def least_squares(word_list, L):
         return mem_dict[(end_space, index)]
     
 
-    least_error = aux(L, 0)
-    line_list = [[]]
-    end_space = L
+    least_error: int = aux(L, 0)
+
+    # Reconstitution de la mise en page offrant un score optimal
+    line_list: list[list[str]] = [[]]
+    end_space: int = L
     for index, word in enumerate(word_list):
+        # Change de ligne en cas de retour, et met à jour la valeur de endspace pour s'assurer de 
+        # parcourir les bonnes branches de l'arbre de récursivité
         if line_break_dict[(end_space, index)]:
             line_list.append([])
             end_space = L - len(word) - 1
@@ -49,9 +59,12 @@ def least_squares(word_list, L):
             end_space -= len(word) + 1
         line_list[-1].append(word)
 
-    line_list = [' '.join(line) for line in line_list]
+    # Converti la liste de ligne d'une liste de listes en une liste de chaînes de caractères,
+    # complète les lignes avec des espaces, puis transforme cette liste en une seule chaîne
+    # paginée, le texte mis en page final
+    line_list: list[str] = [' '.join(line) for line in line_list]
     line_list = [f'{line:{L}}' for line in line_list]
-    result_text = '\n'.join(line_list)
+    result_text: str = '\n'.join(line_list)
     return least_error, result_text
 
 
